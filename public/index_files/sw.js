5d3507adbdeb98598e829313ae48bfb62f8e41f0
josevergara1999 <jose.vergara.diaz.vr@gmail.com>
2026-05-22 14:09:57 -0400

    Add files via upload

---
+++ public/index_files/sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'inmersia-v1';
+const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (r.ok) {
+        const rc = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc));
+      }
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
